2b25a6ecef31a82db1d6be5990cd0d12c86b0809
Mydeen <Mydeen1193@gmail.com>
2024-08-10 21:23:58 +0530

    add file
    add file

---
+++ static-page/src/main.jsx
@@ -0,0 +1,18 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
+// ReactDOM(document.getElementById('root')).render(
+//   <React>
+//     <App />
+//   </React>,
+// )
+
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+React(document.getElementById("root")).render(
+    <h1>Hello</h1>
+
+)
